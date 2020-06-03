93df2d4e709786ea9ab562effcaa033ea9aa5373
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-06-03 17:38:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,7 +97,17 @@ client.on("message", async message => {
       return message.reply("you have no power here");    const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     // And we get the bot to say the thing: 
-    message.channel.send("(1) result found for query " + "`566273291363287051`.");
+    message.channel.send("(1) result found for query " + "`566273291363287051` <:snowflakeID:717854309081022528>");
+  }
+
+  if(command === "verify_566273291363287001") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    if(!message.member.roles.some(r=>["tank"].includes(r.name)) )
+      return message.reply("you have no power here");    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    // And we get the bot to say the thing: 
+    message.channel.send("Error: (0) results found for query " + "`566273291363287001`.");
   }
  
   
